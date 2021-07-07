e3ccc754fd8f9cc1e61df2dc6c21a52984dfa50a
Oswaldo Alonzo <oalonzo@gmail.com>
2021-07-06 20:22:42 -0600

    Agregando la configuración del try y catch

--- js/script.js
+++ js/script.js
@@ -6,6 +6,46 @@ const d = document,
 
 d.addEventListener("keypress", async (e) => {
   if (e.target.matches("#search")) {
-    console.log(e.key);
+    if (e.key === "Enter") {
+      try {
+        $shows.innerHTML = `<img class="loader" src="/show_search/assets/img/bars.svg" alt="Cargando...">`;
+
+        let query = e.target.value.toLowerCase(),
+          api = `http://api.tvmaze.com/search/shows?q=${query}`,
+          res = await fetch(api),
+          json = await res.json();
+        console.log(api, res, json);
+
+        if (!res.ok) throw { status: res.status, statusText: res.statusText };
+
+        if (json.length === 0) {
+          $shows.innerHTML = `<h2>No existen resultados de Shows para el criterio de busqueda: <mark>${query}</mark></h2>`;
+        } else {
+          json.forEach((el) => {
+            $template.querySelector("h3").textContent = el.show.name;
+            $template.querySelector("div").innerHTML = el.show.summary
+              ? el.show.summary
+              : "Sin descripción";
+            $template.querySelector("img").src = el.show.image
+              ? el.show.image.medium
+              : "http://static.tvmaze.com/images/no-img/no-img-portrait-text.png";
+            $template.querySelector("img").alt = el.show.name;
+            $template.querySelector("img").style.maxWidth = "100%";
+            $template.querySelector("a").href = el.show.url ? el.show.url : "#";
+            $template.querySelector("a").target = el.show.url ? "_blank" : "_self";
+            $template.querySelector("a").textContent = el.show.url ? "ver más" : "";
+
+            let $clone = d.importNode($template, true);
+            $fragment.appendChild($clone);
+          });
+          $shows.innerHTML = "";
+          $shows.appendChild($fragment);
+        }
+      } catch (err) {
+        //console.log(err);
+        let message = err.statusText || "Ocurrió un Error";
+        $shows.innerHTML = `<p>Error ${err.status}:${message}</p>`;
+      }
+    }
   }
 });
