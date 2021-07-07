358772f86709812eab9db261a0ed02e1ffdd6333
Oswaldo Alonzo <oalonzo@gmail.com>
2021-07-06 19:09:18 -0600

    cambiando nombre de archivo stiles.css a styles.css

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-|
+//Declarando las variables
+const d = document,
+  $shows = d.getElementById("shows"),
+  $template = d.getElementById("show-template").content,
+  $fragment = d.createDocumentFragment();
+
+d.addEventListener("keypress", async (e) => {
+  if (e.target.matches("#search")) {
+    console.log(e.key);
+  }
+});
